75ec30b7a1e40130536fc49a70a705233d6ad5f4
alexandrawowlol <alexandra.hurtado03@gmail.com>
2018-07-25 16:37:47 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,8 +1,8 @@
-# def fizz_buzz
-# fizz_3 = fizzbuzz(3)
-# fizz_5 = fizzbuzz(5)
-# fizz_15 = fizzbuzz(15)
-# fizz_4 = fizzbuzz(4)
-# end
-# end
-# fizz_buzz
+def fizz_buzz
+fizz_3 = fizzbuzz(3)
+fizz_5 = fizzbuzz(5)
+fizz_15 = fizzbuzz(15)
+fizz_4 = fizzbuzz(4)
+end
+end
+fizz_buzz

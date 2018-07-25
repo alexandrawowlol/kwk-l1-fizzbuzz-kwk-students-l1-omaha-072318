c549c6fd6a146ff941055adf7ab4618677d0366c
alexandrawowlol <alexandra.hurtado03@gmail.com>
2018-07-25 16:52:02 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,10 @@
 def fizzbuzz(number)
   if number % 3 == 0 && number % 5 == 0
     "FizzBuzz"
-    
+    elsif number % 5 == 0
+    "Buzz"
+  else number % 3 == 0
+    "Fizz"
   end
 end
 # fizzbuzz
